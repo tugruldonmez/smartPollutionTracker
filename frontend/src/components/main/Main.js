4260601fb4b4861f4e244a209b04881e77d143c9
tugruldonmez <cavslebron@hotmail.com>
2024-05-08 16:34:12 +0300

    component layout

--- frontend/src/components/main/Main.js
+++ frontend/src/components/main/Main.js
@@ -4,12 +4,15 @@ import Navbar from '../navbar/Navbar';
 
 const Main = () => {
   return (
-    <div id='container' className='w-full h-screen flex flex-col'>
+    <div id='container' className='w-full h-screen flex flex-col overflow-y-auto'>
       <div id='header' className='w-full h-1/4 bg-slate-500'>
-        <Navbar />
+        <div className=' w-full h-fit'>
+            <Navbar />
+        </div>
+        
       </div>
-      <div id='main' className='w-full h-7/12 bg-red-500'></div>
-      <div id='footer' className='w-full h-1/6 bg-amber-400'></div>
+      <div id='main' className='w-full h-fit bg-red-500'></div>
+      <div id='footer' className='w-full h-fit bg-amber-400'></div>
     </div>
   );
 };
