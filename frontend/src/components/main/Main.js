56103b5eea9a9229466bb9bc5533538879668e8e
tugruldonmez <cavslebron@hotmail.com>
2024-05-08 16:52:00 +0300

    component layout

--- frontend/src/components/main/Main.js
+++ frontend/src/components/main/Main.js
@@ -5,13 +5,17 @@ import Navbar from '../navbar/Navbar';
 const Main = () => {
   return (
     <div id='container' className='w-full h-screen flex flex-col overflow-y-auto'>
-      <div id='header' className='w-full h-1/4 bg-slate-500'>
+      <div id='header' className='w-full h-fit bg-slate-500'>
         <div className=' w-full h-fit'>
             <Navbar />
         </div>
-        
       </div>
-      <div id='main' className='w-full h-fit bg-red-500'></div>
+      <div id='main' className='w-full h-fit bg-red-500 p-8'>
+        <div id='topOfMain' className=' text-center'>
+            <p className=' text-7xl font-sans font-bold'>Hava Kirliliği</p>
+            <p className=' text-2xl font-sans font-semibold'>"Görünmez Katil"</p>
+        </div>
+      </div>
       <div id='footer' className='w-full h-fit bg-amber-400'></div>
     </div>
   );
