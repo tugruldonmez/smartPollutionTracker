c38272afcdfa84bc299ae76ad98fd810438a3397
tugruldonmez <cavslebron@hotmail.com>
2024-05-08 15:50:02 +0300

    react folder structure

--- frontend/src/components/main/main.js
+++ frontend/src/components/main/main.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './Main.css'; // Tailwind CSS'i içeren bir dosya olduğunu varsayalım
 
 const Main = () => {
   return (
-    <div>
-       <p>helloworld</p>
+    <div id='container' className='w-full h-full flex'>
+       <div id='header' className='w-full h-1/4 bg-slate-500'></div>
+       <div id='main' className='w-full h-7/12 bg-red-500'></div>
+       <div id='footer' className='w-full h-1/6 bg-amber-400'></div>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
